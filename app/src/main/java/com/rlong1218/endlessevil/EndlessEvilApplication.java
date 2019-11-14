4bee1de53127edbf6a60c8a490f4d447b43691e6
Rlong1218 <7dbrobert1218@gmail.com>
2019-11-14 14:08:41 -0700

    Google Sign In up and running

--- app/src/main/java/com/rlong1218/endlessevil/EndlessEvilApplication.java
+++ app/src/main/java/com/rlong1218/endlessevil/EndlessEvilApplication.java
@@ -3,12 +3,14 @@ package com.rlong1218.endlessevil;
 import android.app.Application;
 import com.facebook.stetho.Stetho;
 import com.rlong1218.endlessevil.service.EndlessEvilDatabase;
+import com.rlong1218.endlessevil.service.GoogleSignInService;
 
 public class EndlessEvilApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
+    GoogleSignInService.setApplicationContext(this);
     Stetho.initializeWithDefaults(this);
     EndlessEvilDatabase.setApplicationContext(this);
     new Thread(() -> EndlessEvilDatabase.getInstance().getCharacterDao().insert()).start();
--- app/src/main/java/com/rlong1218/endlessevil/service/GoogleSignInService.java
+++ app/src/main/java/com/rlong1218/endlessevil/service/GoogleSignInService.java
@@ -2,6 +2,7 @@ package com.rlong1218.endlessevil.service;
     import android.app.Activity;
     import android.app.Application;
     import android.content.Intent;
+    import android.util.Log;
     import androidx.lifecycle.LiveData;
     import androidx.lifecycle.MutableLiveData;
     import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -25,92 +26,32 @@ public class GoogleSignInService {
         .requestEmail()
         .requestId()
         .requestProfile()
-        .requestIdToken(BuildConfig.CLIENT_ID)
+//        .requestIdToken(BuildConfig.CLIENT_ID)
         .build();
     client = GoogleSignIn.getClient(applicationContext, options);
   }
 
-  /**
-   * Sets the context required by a {@link GoogleSignInClient} used by this service.
-   *
-   * @param applicationContext {@link android.content.Context} used for signing in.
-   */
   public static void setApplicationContext(Application applicationContext) {
     GoogleSignInService.applicationContext = applicationContext;
   }
 
-  /**
-   * Returns the singleton instance of this service.
-   */
   public static GoogleSignInService getInstance() {
     return InstanceHolder.INSTANCE;
   }
 
-  /**
-   * Returns {@link LiveData} allowing observation of the currently logged-in {@link
-   * GoogleSignInAccount}.
-   */
   public LiveData<GoogleSignInAccount> getAccount() {
     return account;
   }
 
-  /**
-   * Returns {@link LiveData} allowing observation of the most recent {@link Exception} caught
-   * while attempting to sign in or out.
-   */
   public LiveData<Exception> getException() {
     return exception;
   }
 
-  /**
-   * Refreshes the most recently logged-in credentials, if possible.
-   *
-   * @return asynchronous operation on which completion/success/failures listeners can be set.
-   */
+
   public Task<GoogleSignInAccount> refresh() {
     return client.silentSignIn()
         .addOnSuccessListener(this::update)
         .addOnFailureListener(this::update);
-  }
-
-  /**
-   * Opens an {@link Activity} displaying Google Sign In controls.
-   *
-   * @param activity receiver for result of Google Sign In.
-   * @param requestCode consumer-specifiable request code, passed back with result; should be
-   * checked by receiver.
-   */
-  public void startSignIn(Activity activity, int requestCode) {
-    update((GoogleSignInAccount) null);
-    Intent intent = client.getSignInIntent();
-    activity.startActivityForResult(intent, requestCode);
-  }
-
-  /**
-   * Processes result of Google Sign In operation and returns an asynchronous task.
-   *
-   * @param data payload of result.
-   * @return asynchronous operation on which completion/success/failures listeners can be set.
-   */
-  public Task<GoogleSignInAccount> completeSignIn(Intent data) {
-    Task<GoogleSignInAccount> task = null;
-    try {
-      task = GoogleSignIn.getSignedInAccountFromIntent(data);
-      account.setValue(task.getResult(ApiException.class));
-    } catch (ApiException e) {
-      update(e);
-    }
-    return task;
-  }
-
-  /**
-   * Initiates sign-out and returns asynchronous task.
-   *
-   * @return asynchronous operation on which completion/success/failures listeners can be set.
-   */
-  public Task<Void> signOut() {
-    return client.signOut()
-        .addOnCompleteListener((account) -> update((GoogleSignInAccount) null));
   }
 
   private void update(GoogleSignInAccount account) {
@@ -121,7 +62,32 @@ public class GoogleSignInService {
   private void update(Exception ex) {
     account.setValue(null);
     exception.setValue(ex);
+    Log.e(getClass().getSimpleName(), ex.getMessage(), ex);
   }
+
+  public void startSignIn(Activity activity, int requestCode) {
+    update((GoogleSignInAccount) null);
+    Intent intent = client.getSignInIntent();
+    activity.startActivityForResult(intent, requestCode);
+  }
+
+  public Task<GoogleSignInAccount> completeSignIn(Intent data) {
+    Task<GoogleSignInAccount> task = null;
+    try {
+      task = GoogleSignIn.getSignedInAccountFromIntent(data);
+      update(task.getResult(ApiException.class));
+    } catch (ApiException e) {
+      update(e);
+    }
+    return task;
+  }
+
+//  public Task<Void> signOut() {
+//    return client.signOut()
+//        .addOnCompleteListener((account) -> update((GoogleSignInAccount) null));
+//  }
+
+
 
   private static class InstanceHolder {
 
